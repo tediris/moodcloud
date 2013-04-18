d89d6cfa9cf905b442c27eb6123bb809b02a95f0
tediris <tediris@stanford.edu>
2013-04-17 20:14:21 -0700

    second commit

--- moodcloud.js
+++ moodcloud.js
@@ -1,21 +1,45 @@
 if (Meteor.isClient) {
 
-  next_track = ""
-  currentSound = null
-  trackCount = 0;
+  next_track = "";
+  currentSound = null;
+  var query = "";
+  var trackCount = 0;
+  var playedTracks = [];
 
-  Template.hello.greeting = function () {
-    return "Welcome to moodcloud.";
-  };
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log(next_track);
-        loadNext();
-    }
+  Meteor.startup(function() {
+
+    $('#buttonTest').click(function () {
+      loadNext();
+    });
+
+    console.log("document is ready...");
+
+    $('#searchButton').click(function (){
+      query = $("#query").val();
+      console.log(query);
+      trackCount = 0;
+    });
+
+    $('#query').keypress(function(e) {
+      var code = (e.keyCode ? e.keyCode : e.which);
+      if(code == 13) { //Enter keycode
+        //Do something
+        query = $("#query").val();
+        console.log(query);
+        trackCount = 0;
+      }
+    });
+
   });
+
+  
+
+  setInterval(function () {
+    if (currentSound != null && currentSound.playState == 0){
+      loadNext();
+    }
+  }, 1000);
 
   setTimeout(function () {
     SC.stream("/tracks/293", function(sound){
@@ -27,24 +51,24 @@ if (Meteor.isClient) {
   function loadNext(){
 
     currentSound.stop();
+    currentSound.unload();
 
     SC.stream("/tracks/" + next_track, function(sound){
-      sound.play({
-        onfinish: function() {
-          console.log("track finished");
-          loadNext();
-        }
-      });
+      sound.play();
       currentSound = sound;
     });
 
-    SC.get('/tracks', { q: 'zedd', license: 'cc-by-sa' }, function(tracks) {
+    SC.get('/tracks', { q: query, license: 'cc-by-sa' }, function(tracks) {
+      //console.log(tracks);
+      console.log("trackCount:" + trackCount);
       next_track = tracks[trackCount].uri.split("/");
       next_track = next_track[next_track.length - 1];
+      console.log(next_track);
       trackCount++;
+      //console.log("playd tracks: " + playedTracks);
     });
+  
   }
-
 }
 
 if (Meteor.isServer) {
